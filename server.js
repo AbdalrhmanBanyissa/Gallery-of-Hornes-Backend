7e40e1ae85cace8c4b89f1de4f63d4ca39221001
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-08-15 14:59:29 +0300

    beasts data is added to the server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const server = express()
 
 const cors = require("cors")
+const { request } = require("express")
 server.use(cors())
 
 require("dotenv").config()
@@ -10,7 +11,14 @@ const PORT = process.env.PORT
 server.get("/", (req, res) => {
   res.send("Hello World !")
 })
+/****************************************/
 
+const data = require("./data.json")
+server.get("/data", (req, res) => {
+  res.send(data)
+})
+
+/****************************************/
 server.listen(PORT, () => {
   console.log(`listining on Port ${PORT}`)
 })
