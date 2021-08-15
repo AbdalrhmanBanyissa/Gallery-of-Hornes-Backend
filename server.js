91cee24c28cfb928f9ef4bc6e3b10689cf91db0d
AbdalrhmanBanyissa <aboood.banyissa.94@gmail.com>
2021-08-15 14:24:29 +0300

    initialize the server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const server = express()
+
+const cors = require("cors")
+server.use(cors())
+
+require("dotenv").config()
+const PORT = process.env.PORT
+
+server.get("/", (req, res) => {
+  res.send("Hello World !")
+})
+
+server.listen(PORT, () => {
+  console.log(`listining on Port ${PORT}`)
+})
